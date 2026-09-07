e32eaf971396ee683f2bad2e2c8653c98d9a5106
NjiaMauzo-Afrika <161914591+Njiafilm@users.noreply.github.com>
2026-09-07 13:02:29 +0300

    Implement Kivy app with WebView for Android

---
+++ main.py
@@ -0,0 +1,37 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.utils import platform
+from kivy.clock import Clock
+
+class NjiaMauzoApp(App):
+    def build(self):
+        if platform == "android":
+            Clock.schedule_once(self.load_webview, 1)
+        return Widget()
+
+    def load_webview(self, dt):
+        from jnius import autoclass
+        from android.runnable import run_on_ui_thread
+
+        WebView = autoclass('android.webkit.WebView')
+        WebViewClient = autoclass('android.webkit.WebViewClient')
+        WebChromeClient = autoclass('android.webkit.WebChromeClient')
+        activity = autoclass('org.kivy.android.PythonActivity').mActivity
+
+        @run_on_ui_thread
+        def setup():
+            webview = WebView(activity)
+            settings = webview.getSettings()
+            settings.setJavaScriptEnabled(True)
+            settings.setDomStorageEnabled(True)
+            settings.setLoadWithOverviewMode(True)
+            settings.setUseWideViewPort(True)
+            settings.setDatabaseEnabled(True)
+            webview.setWebViewClient(WebViewClient())
+            webview.setWebChromeClient(WebChromeClient())
+            webview.loadUrl("https://njiamauzo-afrika.onrender.com/")
+            activity.setContentView(webview)
+
+        setup()
+
+NjiaMauzoApp().run()
